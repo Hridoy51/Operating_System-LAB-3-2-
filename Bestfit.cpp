3acffe6368cc7fd45db58f5709cee39407a7bb84
Moshiur Rahman <mrh6627@gmail.com>
2023-03-20 00:25:51 +0600

    Single instance deadlock detection is added

---
+++ Bestfit.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int n, x;
+  string s;
+  cin >> n;
+  vector<pair<int, int>> v;
+  for (int i = 1; i <= n; i++) {
+    cin >> x;
+    cin >> s;
+    v.push_back({x, i});
+  }
+  sort(v.begin(), v.end());
+  int new_task;
+  cin >> new_task >> s;
+  for (auto a : v) {
+    if (a.first >= new_task) {
+      cout << "The new Process is get the free slot number " << a.second
+           << " where the size is " << a.first << " " << s << endl;
+      return 0;
+    }
+  }
+  cout << "There is no free slot to fit the new Process";
+}
---
+++ Single_Instances_Resource_DeadLock.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define vti vector<int>
+using namespace std;
+int p, r;
+int visited[10000];
+vti graph[1000];
+int global, dead = 0;
+void dfs(int x) {
+  if (!visited[x])
+    visited[x] = 1;
+  for (int i = 0; i < graph[x].size(); i++) {
+    if (graph[x][i] == global) {
+      dead = 1;
+      return;
+    }
+    if (!visited[i])
+      dfs(graph[x][i]);
+  }
+}
+int main() {
+  string a, b;
+  cin >> p >> r;
+  vti in_r[r + 1], out_r[r + 1];
+  while (cin >> a >> b) {
+
+    int x = a[1] - 48;
+    int y = b[1] - 48;
+    if (a[0] == 'p') {
+      in_r[y].push_back(x);
+    } else
+      out_r[x].push_back(y);
+  }
+  for (int i = 1; i <= r; i++) {
+    for (int j = 0; j < in_r[i].size(); j++) {
+      for (int k = 0; k < out_r[i].size(); k++) {
+        graph[in_r[i][j]].push_back(out_r[i][k]);
+      }
+    }
+  }
+  //   for (int i = 1; i <= p; i++) {
+  //     cout << "process " << i << " : ";
+  //     for (int j = 0; j < graph[i].size(); j++) {
+  //       cout << graph[i][j] << " ";
+  //     }
+  //     cout << endl;
+  //   }
+  for (int i = 1; i <= p; i++) {
+    for (int j = 0; j <= p; j++)
+      visited[j] = 0;
+    if (dead == 1) {
+      cout << "DEAD LOCK DETECTED ";
+      return 0;
+    }
+    global = i;
+    if (!visited[i]) {
+      dfs(i);
+    }
+  }
+  cout << "THERE IS NO DEADLOCK";
+}
